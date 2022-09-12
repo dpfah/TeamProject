b605e9d50ef7d8ad9246715b810cc49f0921083c
hwonniee <skyred5846@naver.com>
2022-09-13 00:07:08 +0900

    item menu 페이징, 카테고리 나누기 수정중

---
+++ shop-master/src/main/java/com/shop/dto/CookiesItemDto.java
@@ -0,0 +1,35 @@
+package com.shop.dto;
+
+import com.querydsl.core.annotations.QueryProjection;
+import com.shop.constant.ItemType;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class CookiesItemDto {
+
+    private Long id;
+
+    private String itemNm;
+
+    private String itemDetail;
+
+    private String imgUrl;
+
+    private Integer price;
+    
+    private ItemType itemType;
+
+
+    @QueryProjection
+    public CookiesItemDto(Long id, String itemNm, String itemDetail, String imgUrl,Integer price,ItemType itemType){
+        this.id = id;
+        this.itemNm = itemNm;
+        this.itemDetail = itemDetail;
+        this.imgUrl = imgUrl;
+        this.price = price;
+        this.itemType = itemType;
+    }
+
+}
