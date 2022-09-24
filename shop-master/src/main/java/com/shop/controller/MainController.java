5de304523543a7c88ab6f052e31bc1707b0e6dcf
celebi251 <97a123@naver.com>
2022-09-24 14:28:20 +0900

    메인페이지에 빵, 쿠키, 케이크 별로 상품 보이도록 하고, 페이지 우측 아래 바로 이동 버튼 추가

--- shop-master/src/main/java/com/shop/controller/MainController.java
+++ shop-master/src/main/java/com/shop/controller/MainController.java
@@ -3,6 +3,9 @@ package com.shop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.shop.dto.BreadItemDto;
+import com.shop.dto.CakeItemDto;
+import com.shop.dto.CookiesItemDto;
 import com.shop.dto.ItemSearchDto;
 import com.shop.dto.MainItemDto;
 import com.shop.service.ItemService;
@@ -24,10 +27,24 @@ public class MainController {
     @GetMapping(value = "/")
     public String main(ItemSearchDto itemSearchDto, Optional<Integer> page, Model model){
 
-        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 12);
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 3);
         Page<MainItemDto> items = itemService.getMainItemPage(itemSearchDto, pageable);
 
         model.addAttribute("items", items);
+        model.addAttribute("itemSearchDto", itemSearchDto);
+        model.addAttribute("maxPage", 1);
+
+        Pageable pageable2 = PageRequest.of(page.isPresent() ? page.get() : 0, 3);
+        Page<BreadItemDto> breads = itemService.getBreadItemPage(itemSearchDto, pageable2);
+        model.addAttribute("breads", breads);
+        model.addAttribute("itemSearchDto", itemSearchDto);
+        model.addAttribute("maxPage", 1);
+        Page<CookiesItemDto> cookies = itemService.getCookiesItemPage(itemSearchDto, pageable2);
+        model.addAttribute("cookies", cookies);
+        model.addAttribute("itemSearchDto", itemSearchDto);
+        model.addAttribute("maxPage", 1);
+        Page<CakeItemDto> cakes = itemService.getCakeItemPage(itemSearchDto, pageable2);
+        model.addAttribute("cakes", cakes);
         model.addAttribute("itemSearchDto", itemSearchDto);
         model.addAttribute("maxPage", 1);
 
